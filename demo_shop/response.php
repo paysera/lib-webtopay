9995810f6cbf841954f0dd362e4db4ebc7977655
Alexander Zaiets <alexander.zaiets@paysera.net>
2022-04-04 09:52:42 +0300

    removed compability of ~5 versions

--- demo_shop/response.php
+++ demo_shop/response.php
@@ -4,7 +4,7 @@ require_once 'includes/helpers.php';
 require_once 'includes/config.php';
 require_once '../src/includes.php';
 
-$get = removeQuotes($_GET);
+$get = $_GET;
 $answer = isset($get['answer']) ? $get['answer'] : 'cancel';
 
 if ('callback' == $answer) {
