e826a8ba9df7217c21a09a1c4127f0eb708d5138
Oleksandr Gribiennikov <oleksandr.gribiennikov@paysera.net>
2024-04-16 16:09:17 +0300

    Add development environment

---
+++ run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+container='lib_webtopay_tests'
+
+if [ "$(docker ps -a -q -f name=$container)" ]; then
+    docker stop $container
+    docker rm $container
+fi
+docker build -t $container -f $PWD/Dockerfile $PWD
+
+docker run -d --name $container -v $PWD:/var/www -w /var/www $container
+docker exec $container bash -c "composer i"
+
+#docker exec -it $container bash
+docker exec $container bash -c "composer run phpunit"
+docker container stop $container
+#docker container rm --force $container
+#docker image rm $container
