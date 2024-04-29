506ebbf25ffd81c9681990a726ee498917ea3183
Oleksandr Gribiennikov <oleksandr.gribiennikov@paysera.net>
2024-04-29 12:47:15 +0300

    Update tests running script

--- run_tests.sh
+++ run_tests.sh
@@ -6,12 +6,12 @@ if [ "$(docker ps -a -q -f name=$container)" ]; then
     docker stop $container
     docker rm $container
 fi
-docker build -t $container -f $PWD/Dockerfile $PWD
+docker build -t $container -f $PWD/Dockerfile --build-arg PHP_VER=7.4 $PWD
 
 docker run -d --name $container -v $PWD:/var/www -w /var/www $container
-docker exec $container bash -c "composer i"
+#docker exec $container bash -c "composer i"
 
-#docker exec -it $container bash
+docker exec -it $container bash
 docker exec $container bash -c "composer run phpunit"
 docker container stop $container
 #docker container rm --force $container
