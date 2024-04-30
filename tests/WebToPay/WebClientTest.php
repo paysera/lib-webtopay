08fd26a9ba6450980af3c5fd2d460f9475ba95f8
Oleksandr Gribiennikov <oleksandr.gribiennikov@paysera.net>
2024-04-30 09:06:42 +0300

    Update `WebClient` and its tests; update build

--- tests/WebToPay/WebClientTest.php
+++ tests/WebToPay/WebClientTest.php
@@ -20,7 +20,7 @@ class WebToPay_WebClientTest extends TestCase
     {
         $this->webClientMock->expects($this->once())
             ->method('openSocket')
-            ->with('example.com', 443)
+            ->with('ssl://example.com', 443)
             ->willReturn(false);
 
         $this->expectException(WebToPayException::class);
@@ -28,24 +28,48 @@ class WebToPay_WebClientTest extends TestCase
         $this->webClientMock->get('https://example.com', ['param1' => 'value1', 'param2' => 'value2']);
     }
 
-    public function testGet()
+    public function getDataForTestingGet(): iterable
+    {
+        yield 'not secure' => [
+            'uri' => '',
+            'host' => '',
+            'host prefix' => '',
+            'port' => 80,
+            'scheme' => 'http',
+        ];
+
+        yield 'secure' => [
+            'uri' => 'https://example.com',
+            'host' => 'example.com',
+            'host prefix' => 'ssl://',
+            'port' => 443,
+            'scheme' => 'https',
+        ];
+    }
+
+    /**
+     * @dataProvider getDataForTestingGet
+     *
+     * @throws WebToPayException
+     */
+    public function testGet(string $uri, string $host, string $hostPrefix, int $port, string $scheme)
     {
         $this->webClientMock->expects($this->once())
             ->method('openSocket')
-            ->with('example.com', 443)
+            ->with($hostPrefix . $host, $port)
             ->willReturn('socket');
 
         $this->webClientMock->expects($this->once())
             ->method('getContentFromSocket')
             ->with(
                 'socket',
-                "GET /?param1=value1&param2=value2 HTTP/1.1\r\nHost: example.com\r\nConnection: Close\r\n\r\n"
+                "GET ?param1=value1&param2=value2 HTTP/1.0\r\nHost: {$host}\r\nConnection: Close\r\n\r\n"
             )
             ->willReturn("HTTP/1.1 200 OK\r\n\r\nContent");
 
         $this->assertEquals(
             'Content',
-            $this->webClientMock->get('https://example.com', ['param1' => 'value1', 'param2' => 'value2'])
+            $this->webClientMock->get($uri, ['param1' => 'value1', 'param2' => 'value2'])
         );
     }
 }
